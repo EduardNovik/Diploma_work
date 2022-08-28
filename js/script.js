f478a77a5349166f23a15876780325a843b38815
Eduard <eddnovik@gmail.com>
2022-08-28 19:12:17 +0200

    adding

---
+++ js/script.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+    var swiper = new Swiper(".mySwiper", {
+        cssMode: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            bulletClass: "custom-bullet",
+            clickable:true,
+        },
+    
+    });
+}
